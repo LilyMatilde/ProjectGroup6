d79c702bf9f0c3d1b39b47c7c6666300201e1d8e
Lily Martin <119932838+LilyMatilde@users.noreply.github.com>
2024-05-13 22:42:53 +0100

    Added image css in parks opacity and JS
    JS for images when clicked to be bigger

--- script.js
+++ script.js
@@ -6,3 +6,37 @@ const nav = document.getElementById("myLinks");
 button.addEventListener("click", () => {
   nav.classList.toggle("show");
 });
+//Parks page images alert pop up box when on click with information what we think of each park
+
+// const serengeti = document.getElementById("serengeti");
+
+// function stats() {
+//   if (serengeti) {
+//     alert("Hello");
+//   } else {
+//     alert("oh no");
+//   }
+// }
+// console.log(stats(serengeti));
+
+// Get the modal
+const modal = document.getElementById("myModal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+const serengeti = document.getElementById("serengeti");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+serengeti.onclick = function () {
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.id;
+  console.log("test");
+};
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+};
